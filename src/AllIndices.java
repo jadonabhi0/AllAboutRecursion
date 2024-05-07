6a3fad54ee1f364b5b0ca530c83ae36674083a6a
Abhishek jadon <61266465+jadonabhi0@users.noreply.github.com>
2024-05-07 21:33:54 +0530

    Update AllIndices.java

--- src/AllIndices.java
+++ src/AllIndices.java
@@ -7,8 +7,6 @@ public class AllIndices {
             return  new int[fsf] ;
 
         }
-
-        
         if(array[index] == num){
             int[] arr = allIndices(array,num,index+1,fsf+1);
             arr[fsf] = index;
